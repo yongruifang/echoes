33e04a610eeb571728d525ec2cb590f07cf18eb8
yongruifang <yongruifang@163.com>
2023-12-19 23:08:09 +0800

    TopBar组件化

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,11 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
+import TopBar from './components/TopBar.vue';
 </script>
 
 <template>
   <main>
-    <header>
-      <div class="logo-block">
-        <img alt="Vue logo" class="logo" src="@/assets/moment-echoes.svg" width="50" height="50" />
-        <p>Echoes</p>
-      </div>
-      <div class="wrapper">
-        <nav>
-          <RouterLink to="/">留言墙</RouterLink>
-          <RouterLink to="/photo">照片墙</RouterLink>
-        </nav>
-      </div>
-    </header>
+    <TopBar />
     <RouterView />
   </main>
 </template>
@@ -25,49 +15,4 @@ main {
   width: 100%;
   height: 100%;
 }
-
-header {
-  display: flex;
-  align-items: center;
-  padding: 0 20px;
-  height: 80px;
-  background-color: #fff;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-}
-
-header .logo-block {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 25%;
-
-  p {
-    margin-left: 10px;
-    font-size: 20px;
-    font-weight: 600;
-  }
-}
-
-header .wrapper {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 75%;
-}
-
-nav a {
-  /* background-color: #3B73F0; */
-  color: #3b73f0;
-  padding: 10px 20px;
-  border-radius: 50px;
-  margin: 0 10px;
-}
-
-nav a.router-link-active {
-  background-color: #3b73f0;
-  color: #fff;
-}
-
-
-@media (min-width: 1024px) {}
 </style>
---
+++ src/components/TopBar.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+<template>
+    <header>
+        <div class="logo-block">
+            <img alt="Vue logo" class="logo" src="@/assets/moment-echoes.svg" width="50" height="50" />
+            <p>Echoes</p>
+        </div>
+        <div class="wrapper">
+            <nav>
+                <RouterLink to="/">留言墙</RouterLink>
+                <RouterLink to="/photo">照片墙</RouterLink>
+            </nav>
+        </div>
+    </header>
+</template>
+<style scoped>
+header {
+    display: flex;
+    align-items: center;
+    padding: 0 20px;
+    height: 80px;
+    background-color: #fff;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+header .logo-block {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 25%;
+
+    p {
+        margin-left: 10px;
+        font-size: 20px;
+        font-weight: 600;
+    }
+}
+
+header .wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 75%;
+}
+
+nav a {
+    /* background-color: #3B73F0; */
+    color: #3b73f0;
+    padding: 10px 20px;
+    border-radius: 50px;
+    margin: 0 10px;
+}
+
+nav a.router-link-active {
+    background-color: #3b73f0;
+    color: #fff;
+}
+
+
+@media (min-width: 1024px) {}
+</style>
